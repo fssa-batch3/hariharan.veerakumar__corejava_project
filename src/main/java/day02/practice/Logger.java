696be1f76b8d6ca868a04fe40d012b3250c5de7e
Smarthariharan <116737971+Smarthariharan@users.noreply.github.com>
2023-07-11 16:53:33 +0530

    day 02 practice completed

--- src/main/java/day02/practice/Logger.java
+++ src/main/java/day02/practice/Logger.java
@@ -2,9 +2,47 @@ package day02.practice;
 
 public class Logger {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		public static void debug (String msg) {
+			System.out.println("DEBUG: " + msg);
+		}
+		public static void info (String msg) {
+			System.out.println("INFO: " + msg);
+		}
+		
+		public static void error (String msg) {
+			System.out.println("ERROR: " + msg);
+		}
+		
+		public static void debug (int num) {
+			System.out.println("DEBUG: " + num);
+		}
+		public static void info (int num) {
+			System.out.println("INFO: " + num);
+		}
+		public static void error (int num) {
+			System.out.println("ERROR: " + num);
+		}
+		public static void debug (float number) {
+			System.out.println("DEBUG: " + number);
+		}
+		public static void info (float number) {
+			System.out.println("INFO: " + number);
+		}
+		public static void error (float number) {
+			System.out.println("ERROR: " + number);
+		}
+		public static void debug (boolean number) {
+			System.out.println("DEBUG: " + number);
+		}
+		public static void info (boolean number) {
+			System.out.println("INFO: " + number);
+		}
+		public static void error (boolean number) {
+			System.out.println("ERROR: " + number);
+		}
+		
+
+	
 
 }
